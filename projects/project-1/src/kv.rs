b872b53a5ef69ce11d7887c9df0653ac2e74f557
shaojintian <sjt@hnu.edu.cn>
2020-02-05 20:27:42 +0800

    init project1

--- projects/project-1/src/kv.rs
+++ projects/project-1/src/kv.rs
@@ -1,15 +1,2 @@
 use std::collections::HashMap;
 
-/// The `KvStore` stores string key/value pairs.
-///
-/// Key/value pairs are stored in a `HashMap` in memory and not persisted to disk.
-///
-/// Example:
-///
-/// ```rust
-/// # use kvs::KvStore;
-/// let mut store = KvStore::new();
-/// store.set("key".to_owned(), "value".to_owned());
-/// let val = store.get("key".to_owned());
-/// assert_eq!(val, Some("value".to_owned()));
-/// ```
